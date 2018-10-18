4376c4f938aa3aee4bce4b17f8b174331949b4f7
Jim McLean <jim_mclean@optusnet.com.au>
2018-10-19 10:27:26 +1100

    Verbose test

--- tests/testthat/test_download.R
+++ tests/testthat/test_download.R
@@ -6,13 +6,13 @@ test_that("JDownload works", {
   .prepare()
 
   file <- JDownload("https://farm5.staticflickr.com/4507/37847388931_959d812490_o_d.jpg",
-                    cacheDir = TEST_DIR)
+                    cacheDir = TEST_DIR, verbose = TRUE)
   expect_true(!is.null(file))
   expect_true(file.exists(file))
   info <- file.info(file)
 
   file2 <- JDownload("https://farm5.staticflickr.com/4507/37847388931_959d812490_o_d.jpg",
-                    cacheDir = TEST_DIR)
+                    cacheDir = TEST_DIR, verbose = TRUE)
   expect_true(!is.null(file2))
   expect_true(file.exists(file2))
   info2 <- file.info(file)
