b02297ca09759d2c569f014aa73cfdd3f2538771
Jim McLean <jim_mclean@optusnet.com.au>
2021-04-19 14:56:21 +1000

    Fix test

--- tests/testthat/test_file_plot.R
+++ tests/testthat/test_file_plot.R
@@ -172,7 +172,7 @@ test_that("pdf plotting", {
   img <- tf("test2.pdf")
   JPlotToPDF(img, plotWigglyLines(), width = width, height = height, units = units, embedFonts = TRUE)
   expect_true(file.exists(img))
-  lines <- readLines(img, n = 6)
+  lines <- readLines(img, n = 3) # Don't get too many lines or there are embedded NULs that cause an error
 })
 
 test_that("svg plotting", {
